12a26c4361877847afe56b48aa4e3150deba0344
Stilllee <orodae@gmail.com>
2023-06-22 01:28:46 +0900

    #4.2 Test Drive part Two

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,28 +2,49 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-20 px-10 grid gap-10 text-black">
-      <div className="bg-white p-6 rounded-3xl shadow-xl  ">
-        <span className="font-semibold text-3xl">Select Item</span>
+    <div className="grid gap-10 px-20 py-20 font-semibold text-black bg-slate-400">
+      <div className="p-6 bg-white shadow-xl rounded-3xl ">
+        <span className="text-3xl ">Select Item</span>
         <div className="flex justify-between my-2">
           <span className="text-gray-500">Grey Chair</span>
-          <span className="font-semibold">$19</span>
+          <span>$19</span>
         </div>
         <div className="flex justify-between">
           <span className="text-gray-500">Tooly Table</span>
-          <span className="font-semibold">$80</span>
+          <span>$80</span>
         </div>
-        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+        <div className="flex justify-between pt-2 mt-2 border-t-2 border-dashed">
           <span>Total</span>
-          <span className="font-semibold">$10</span>
+          <span>$99</span>
         </div>
-        <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/4 mx-auto">
+        <div className="w-2/4 p-3 mx-auto mt-5 text-center text-white bg-blue-500 rounded-xl">
           Checkout
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl  "></div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl  "></div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl  "></div>
+      <div className="overflow-hidden bg-white shadow-xl rounded-3xl">
+        <div className="p-6 bg-blue-500 pb-14">
+          <span className="text-2xl text-white">Profile</span>
+        </div>
+        <div className="relative p-6 bg-white rounded-3xl -top-5">
+          <div className="relative flex items-end justify-between -top-16">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-400">Orders</span>
+              <span>340</span>
+            </div>
+            <div className="w-24 h-24 bg-red-400 rounded-full" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-400">Spent</span>
+              <span>$2,310</span>
+            </div>
+          </div>
+          <div className="relative flex flex-col items-center -mt-10 -mb-5">
+            <span className="text-lg ">Tony Molloy</span>
+            <span className="text-sm text-gray-500">미국</span>
+          </div>
+        </div>
+      </div>
+      <div className="p-6 bg-white shadow-xl rounded-3xl "></div>
+      <div className="p-6 bg-white shadow-xl rounded-3xl "></div>
     </div>
   );
 };
