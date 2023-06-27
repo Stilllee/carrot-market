5baf11e7ffe2cc2efaef52da4909e74160e6a43b
Stilllee <orodae@gmail.com>
2023-06-27 23:20:53 +0900

    #4.8 More Modifiers

--- README.md
+++ README.md
@@ -37,7 +37,7 @@
 - [x] #4.5 Transitions
 - [x] #4.6 Modifiers for Lists
 - [x] #4.7 Modifiers for Forms
-- [ ] #4.8 More Modifiers
+- [x] #4.8 More Modifiers
 - [ ] #4.9 Responsive Modifiers
 - [ ] #4.10 Responsive Modifiers part Two
 - [ ] #4.11 Dark Mode
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,24 +2,11 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <form className="flex flex-col p-5 space-y-2 text-black">
-      <input
-        type="text"
-        required
-        placeholder="Username"
-        className="p-1 border border-gray-400 rounded-md peer"
-      />
-      <span className="hidden text-white peer-invalid:block peer-invalid:text-red-500">
-        Thie input is invalid
-      </span>
-      <span className="hidden text-white peer-valid:block peer-valid:text-teal-500">
-        Awesome username
-      </span>
-      <span className="hidden text-white peer-hover:block peer-valid:text-amber-500">
-        Hello
-      </span>
-      <input type="submit" value="Login" className="bg-white" />
-    </form>
+    <div className="flex flex-col h-screen p-5 space-y-2 text-black bg-white">
+      <p className="first-letter:text-7xl first-letter:hover:text-purple-400">
+        Hello everyone!
+      </p>
+    </div>
   );
 };
 
