f99e44e52df1be6a815bc15c55b797bb7e91b2c8
Stilllee <orodae@gmail.com>
2023-06-28 01:15:36 +0900

    #4.12 Just In Time Compiler

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,87 +2,8 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="grid min-h-screen gap-10 px-20 py-20 font-semibold text-black xl:place-content-center lg:grid-cols-2 xl:grid-cols-3 bg-slate-400">
-      <div className="flex flex-col justify-between p-6 bg-white shadow-xl rounded-3xl dark:bg-black">
-        <span className="text-3xl dark:text-white">Select Item</span>
-        <ul>
-          <div className="flex justify-between my-2">
-            <span className="text-gray-500 dark:text-gray-100">Grey Chair</span>
-            <span className="dark:text-white">$19</span>
-          </div>
-          <div className="flex justify-between">
-            <span className="text-gray-500 dark:text-gray-100">
-              Tooly Table
-            </span>
-            <span className="dark:text-white">$80</span>
-          </div>
-        </ul>
-        <div className="flex justify-between pt-2 mt-2 border-t-2 border-dashed">
-          <span>Total</span>
-          <span>$99</span>
-        </div>
-        <button className="block w-2/4 p-3 mx-auto mt-5 text-center text-white bg-blue-500 rounded-xl dark:hover:bg-white hover:bg-teal-500 dark:hover:text-black hover:text-black active:bg-yellow-500 focus:bg-red-500 dark:bg-black dark:border-white dark:border">
-          Checkout
-        </button>
-      </div>
-      <div className="overflow-hidden bg-white shadow-xl rounded-3xl group">
-        <div className="p-6 pb-14 xl:pb-40 landscape:bg-teal-500 portrait:bg-indigo-600">
-          <span className="text-2xl text-white">Profile</span>
-        </div>
-        <div className="relative p-6 bg-white rounded-3xl -top-5">
-          <div className="relative flex items-end justify-between -top-16">
-            <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-400">Orders</span>
-              <span>340</span>
-            </div>
-            <div className="w-24 h-24 bg-gray-300 rounded-full group-hover:bg-red-300" />
-            <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-400">Spent</span>
-              <span>$2,310</span>
-            </div>
-          </div>
-          <div className="relative flex flex-col items-center -mt-10 -mb-5">
-            <span className="text-lg ">Tony Molloy</span>
-            <span className="text-sm text-gray-500">미국</span>
-          </div>
-        </div>
-      </div>
-      <div className="p-6 bg-white shadow-xl lg:col-span-2 xl:col-span-1 rounded-3xl">
-        <div className="flex items-center justify-between mb-5">
-          <span className="text-2xl">&larr;</span>
-          <div className="space-x-3">
-            <span>⭐4.9</span>
-            <span className="p-2 rounded-md shadow-lg">❤️</span>
-          </div>
-        </div>
-        <div className="bg-zinc-400 h-72" />
-        <div className="flex flex-col">
-          <span className="text-xl ">Swoon Lounge</span>
-          <span className="text-xs text-gray-500">Chair</span>
-          <div className="flex items-center justify-between mt-3 mb-5">
-            <div className="space-x-2">
-              <button className="w-5 h-5 transition bg-yellow-500 rounded-full focus:ring-2 ring-offset-2 ring-yellow-500" />
-              <button className="w-5 h-5 transition bg-indigo-500 rounded-full focus:ring-2 ring-offset-2 ring-indigo-500" />
-              <button className="w-5 h-5 transition bg-teal-500 rounded-full focus:ring-2 ring-offset-2 ring-teal-500" />
-            </div>
-            <div className="flex items-center space-x-5">
-              <button className="rounded-lg p-1.5 bg-blue-100 flex justify-center items-center aspect-square w-10 text-xl text-gray-500">
-                -
-              </button>
-              <span>1</span>
-              <button className="rounded-lg p-1.5 bg-blue-100 flex justify-center items-center aspect-square w-10 text-xl text-gray-500">
-                +
-              </button>
-            </div>
-          </div>
-          <div className="flex items-center justify-between">
-            <span className="text-2xl">$450</span>
-            <button className="px-8 py-2 text-xs text-center text-white bg-blue-500 rounded-lg">
-              Add to cart
-            </button>
-          </div>
-        </div>
-      </div>
+    <div className="dark:md:hover:bg-teal-400 bg-[url('/vercel.svg')]">
+      <h2 className="text-[100px] text-[#82ccdd]">Hello</h2>
     </div>
   );
 };
