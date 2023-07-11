e7a028642fb23796c184c6bbfd18190aa7f22013
Stilllee <orodae@gmail.com>
2023-07-11 19:48:50 +0900

    #9.4 Sending SMS

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,6 +1,9 @@
+import twilio from "twilio";
 import client from "@/libs/server/client";
 import withHandler, { ResponseType } from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
@@ -26,6 +29,19 @@ async function handler(
       },
     },
   });
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE!,
+      /*  
+      // 실제 서비스에서는 phone을 작성해야 하지만, twilio trial 요금제를 쓰고 있기 때문에 그냥 내 핸드폰 번호만을 적어놓음
+      // typescript에게 MY_PHONE은 확실히 존재하는 변수라고 알려주기 위해 !를 붙임 
+      */
+      from: "+12345162429",
+      body: `Your login token is ${payload}`,
+    });
+    console.log(message);
+  }
   return res.json({
     ok: true,
   });
